51d56709f7eb2d701f716ca557375724cd9962be
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-05-13 18:08:32 +0300

    created constants for different types of users

---
+++ Talento-Backend/Talento/src/main/java/iso53/talento/util/Constants.java
@@ -0,0 +1,6 @@
+package iso53.talento.util;
+
+public class Constants {
+    public static final String ROLE_APPLICANT = "applicant";
+    public static final String ROLE_HUMAN_RESOURCES = "human_resources";
+}
