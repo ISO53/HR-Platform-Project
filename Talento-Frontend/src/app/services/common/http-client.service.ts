b4470de2023b1bee54cb19b0563124ed8cc7f9d0
smlozaydin <samil.bilal.ozaydin@gmail.com>
2024-05-15 08:20:47 +0300

    custom http-client service

--- Talento-Frontend/src/app/services/common/http-client.service.ts
+++ Talento-Frontend/src/app/services/common/http-client.service.ts
@@ -1,37 +1,67 @@
 import { Inject, Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor(private httpService: HttpClient, @Inject("baseUrl") baseUrl:string) { }
+  constructor(
+    private httpClient : HttpClient, 
+    @Inject("baseUrl") private baseUrl : string) {   }
   
-  getUrl(){
+  private url(requestParameters:Partial<RequestParameters>):string{
 
+    return `${requestParameters.baseUrl ? requestParameters.baseUrl: this.baseUrl}/${requestParameters.controller}${requestParameters.action ? "/"+requestParameters.action: ""}`;
   }
 
-  get(requestParameters : Partial<RequestParameters>){
-    var geturl = "";
-    if(!requestParameters.fullUrl){
-      
+  get<T>(requestParameters: Partial<RequestParameters>, id?:string): Observable<T>{
+    let url : string ="";
+    if(requestParameters.fullEndPoint){
+      url =requestParameters.fullEndPoint;
+    }else{
+      url =`${this.url(requestParameters)}${id ? id : ""}${requestParameters.queryString ?`?${requestParameters.queryString}`:``}`;
     }
+    return this.httpClient.get<T>(url,{headers: requestParameters.headers});
   }
+  post<T>(requestParameters: Partial<RequestParameters>, body: Partial<T>):Observable<T>{
+    let url : string ="";
+    if(requestParameters.fullEndPoint){
+      url =requestParameters.fullEndPoint;
+    }else{
+      url =`${this.url(requestParameters)}${requestParameters.queryString ?`?${requestParameters.queryString}`:``}`;
+    }
+    return this.httpClient.post<T>(url,body,{headers:requestParameters.headers});
 
-  post(){}
-
-  put(){}
-  
-  delete(){}
+  }
+  put<T>(requestParameters: Partial<RequestParameters>, body: Partial<T>):Observable<T>{
+    let url : string ="";
+    if(requestParameters.fullEndPoint){
+      url =requestParameters.fullEndPoint;
+    }else{
+      url =`${this.url(requestParameters)}${requestParameters.queryString ?`?${requestParameters.queryString}`:``}`;
+    }
+    return this.httpClient.put<T>(url,body,{headers:requestParameters.headers});
+  }
+  delete<T>(requestParameters: Partial<RequestParameters>, id: string):Observable<T>{
+    let url : string ="";
+    if(requestParameters.fullEndPoint){
+      url =requestParameters.fullEndPoint;
+    }else{
+      url =`${this.url(requestParameters)}/${id}${requestParameters.queryString ?`?${requestParameters.queryString}`:``}`;
+    }
+    return this.httpClient.delete<T>(url,{headers:requestParameters.headers});
+  }
 }
 
 export class RequestParameters{
-  controller ?: string;
-  action ?: string;
+  controller?:string;
+  action?: string;
+  id?:string;
+  queryString?:string;
 
-  headers ?: HttpHeaders;
-  baseUrl ?: string;
-  fullUrl ?: string;
-
+  headers?: HttpHeaders;
+  baseUrl?: string;
+  fullEndPoint?: string;
 }
