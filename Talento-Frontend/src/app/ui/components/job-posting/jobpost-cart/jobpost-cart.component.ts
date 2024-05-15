7b5c2b6347ba64ce0371d5dc2176e334985f3b94
smlozaydin <samil.bilal.ozaydin@gmail.com>
2024-05-15 17:24:09 +0300

    added jobpost cart

--- Talento-Frontend/src/app/ui/components/job-posting/jobpost-cart/jobpost-cart.component.ts
+++ Talento-Frontend/src/app/ui/components/job-posting/jobpost-cart/jobpost-cart.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-jobpost-cart',
   templateUrl: './jobpost-cart.component.html',
-  styleUrls: ['./jobpost-cart.component.scss']
+  styleUrls: ['./jobpost-cart.component.scss'],
+
 })
 export class JobpostCartComponent {
 
