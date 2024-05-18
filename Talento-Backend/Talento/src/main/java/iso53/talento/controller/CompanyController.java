bf16e82f91848d9f43a455de91a807a10980660e
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-05-18 14:17:45 +0300

    added endpoint for adding user to company

--- Talento-Backend/Talento/src/main/java/iso53/talento/controller/CompanyController.java
+++ Talento-Backend/Talento/src/main/java/iso53/talento/controller/CompanyController.java
@@ -1,7 +1,9 @@
 package iso53.talento.controller;
 
 import iso53.talento.model.Company;
+import iso53.talento.model.User;
 import iso53.talento.service.CompanyService;
+import iso53.talento.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,9 @@ public class CompanyController {
 
     @Autowired
     private CompanyService companyService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/getAll")
     public ResponseEntity<List<Company>> getAllCompanies() {
@@ -37,6 +42,31 @@ public class CompanyController {
             Company _company = companyService.save(new Company(companyName));
 
             return new ResponseEntity<>(_company, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/addUser")
+    public ResponseEntity<?> addUser(@RequestParam String companyId, @RequestParam String userId) {
+        try {
+            User user = userService.findById(new ObjectId(userId));
+
+            if (user == null) {
+                return new ResponseEntity<>("User not found!", HttpStatus.NOT_FOUND);
+            }
+
+            Company company = companyService.findById(new ObjectId(companyId));
+
+            if (company == null) {
+                return new ResponseEntity<>("Company not found", HttpStatus.NOT_FOUND);
+            }
+
+            company.getUserIDs().add(new ObjectId(userId));
+
+            Company _company = companyService.save(company);
+
+            return new ResponseEntity<>(_company, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
