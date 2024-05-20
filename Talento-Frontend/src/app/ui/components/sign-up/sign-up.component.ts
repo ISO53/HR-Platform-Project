7cdc4c3730ee938b5b9711c2d0a520b1191ed9b6
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-05-20 19:30:51 +0300

    added register functionality

--- Talento-Frontend/src/app/ui/components/sign-up/sign-up.component.ts
+++ Talento-Frontend/src/app/ui/components/sign-up/sign-up.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  fullName: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient, private snackBar: MatSnackBar, private router : Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  signUp(fullName: string, email: string, password: string): void {
+    const url = 'http://localhost:8080/user/sign-up';
+    this.http.post<any>(url, {
+      fullName,
+      email,
+      password
+    }).subscribe(response => {
+      console.log(response);
+      this.snackBar.open('Sign up successful', 'Close', {
+        duration: 2000,
+      });
+      this.router.navigate(['/log-in']); // navigate to login page
+    }, error => {
+      console.error(error);
+      this.snackBar.open('Sign up failed. ' + error.error.toString(), 'Close', {
+        duration: 2000,
+      });
+    });
+  }
 }
--- Talento-Frontend/src/app/ui/components/sign-up/sign-up.module.ts
+++ Talento-Frontend/src/app/ui/components/sign-up/sign-up.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up.component';
 import { RouterModule } from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 
 
 @NgModule({
   declarations: [SignUpComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([{path:"",component:SignUpComponent}])
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild([{path: "", component: SignUpComponent}]),
+        FormsModule
+    ]
 })
 export class SignUpModule { }
