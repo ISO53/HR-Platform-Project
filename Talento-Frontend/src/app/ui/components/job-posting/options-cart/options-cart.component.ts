4f3acd490fff1cc8106694c1c2391d3c3cb00027
smlozaydin <samil.bilal.ozaydin@gmail.com>
2024-05-20 23:42:27 +0300

    added primitive job-posting search ui

--- Talento-Frontend/src/app/ui/components/job-posting/options-cart/options-cart.component.ts
+++ Talento-Frontend/src/app/ui/components/job-posting/options-cart/options-cart.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
 
 @Component({
   selector: 'app-options-cart',
   templateUrl: './options-cart.component.html',
-  styleUrls: ['./options-cart.component.scss']
+  styleUrls: ['./options-cart.component.scss'],
 })
 export class OptionsCartComponent {
 
