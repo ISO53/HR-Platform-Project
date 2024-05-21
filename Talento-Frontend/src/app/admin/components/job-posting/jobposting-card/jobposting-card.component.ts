be5cac7150469b05723f5f04b0d178d257edf11e
smlozaydin <samil.bilal.ozaydin@gmail.com>
2024-05-21 20:37:38 +0300

    added hr job-posting-card

---
+++ Talento-Frontend/src/app/admin/components/job-posting/jobposting-card/jobposting-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobposting-card',
+  templateUrl: './jobposting-card.component.html',
+  styleUrls: ['./jobposting-card.component.scss']
+})
+export class JobpostingCardComponent {
+
+}
