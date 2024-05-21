77a005aaec8566750309a4e95297db0cb8aeab3f
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-05-21 14:08:33 +0300

    connected job-listing page to backend

--- Talento-Frontend/src/app/ui/components/job-posting/job-posting.component.ts
+++ Talento-Frontend/src/app/ui/components/job-posting/job-posting.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-job-posting',
   templateUrl: './job-posting.component.html',
   styleUrls: ['./job-posting.component.scss']
 })
-export class JobPostingComponent {
+export class JobPostingComponent implements OnInit {
+  adverts: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:8080/advert/getAllDetailed').subscribe(response => {
+      this.adverts = response;
+    });
+  }
 
 }
