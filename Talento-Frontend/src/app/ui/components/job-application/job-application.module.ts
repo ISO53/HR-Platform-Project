9c6ff0668fd156b56f58eb0ba6312e35e7e0a346
smlozaydin <samil.bilal.ozaydin@gmail.com>
2024-05-21 21:40:34 +0300

    created ui job-application

---
+++ Talento-Frontend/src/app/ui/components/job-application/job-application.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JobApplicationComponent } from './job-application.component';
+
+
+
+@NgModule({
+  declarations: [
+    JobApplicationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class JobApplicationModule { }
