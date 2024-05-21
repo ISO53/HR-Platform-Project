7dad06a649af58c214bbb7471f3490990f611dc6
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-05-21 22:12:16 +0300

    advert page connected to backend

--- Talento-Frontend/src/app/admin/components/job-posting/jobposting-card/jobposting-card.component.ts
+++ Talento-Frontend/src/app/admin/components/job-posting/jobposting-card/jobposting-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-jobposting-card',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./jobposting-card.component.scss']
 })
 export class JobpostingCardComponent {
-
+  @Input() advert: any;
 }
