98385fea7c3339863f7f0e9fc7965d58c1c4a4f1
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-05-21 17:44:28 +0300

    dashboard panel updated

--- Talento-Frontend/src/app/admin/components/dashboard/dashboard.component.ts
+++ Talento-Frontend/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  constructor(private router: Router) {}
 
+  navigateToJobPosting() {
+    this.router.navigate(['/admin/job-posting']);
+  }
+
+  navigateToTalentTest() {
+    this.router.navigate(['/admin/skill-test']);
+  }
+
+  navigateToApplicants() {
+    this.router.navigate(['/admin/application']);
+  }
+
+  navigateToCalendar() {
+    this.router.navigate(['/admin/calendar']);
+  }
 }
