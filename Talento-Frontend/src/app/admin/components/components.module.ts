dcc04ac9b6e6209db38602b37d61cd198ac07c46
smlozaydin <samil.bilal.ozaydin@gmail.com>
2024-05-23 02:00:56 +0300

    added new job posting

--- Talento-Frontend/src/app/admin/components/components.module.ts
+++ Talento-Frontend/src/app/admin/components/components.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { SkillTestsModule } from './skill-tests/skill-tests.module';
+import { AnalyzesModule } from './analyzes/analyzes.module';
+import { ApplicationModule } from './application/application.module';
+import { CalendarModule } from './calendar/calendar.module';
+import { PoliciesModule } from './policies/policies.module';
 
 
 
@@ -11,7 +15,11 @@ import { SkillTestsModule } from './skill-tests/skill-tests.module';
   imports: [
     CommonModule,
     DashboardModule,
-    SkillTestsModule
+    SkillTestsModule,
+    AnalyzesModule,
+    ApplicationModule,
+    CalendarModule,
+    PoliciesModule
   ]
 })
 export class ComponentsModule { }
--- Talento-Frontend/src/app/admin/components/job-posting/job-create/job-create.component.ts
+++ Talento-Frontend/src/app/admin/components/job-posting/job-create/job-create.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-job-create',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./job-create.component.scss']
 })
 export class JobCreateComponent {
+  addOnBlur = true;
+  readonly separatorKeysCodes = [13, 188] as const;
+  announcer = inject(LiveAnnouncer);
 
+  skills: any[] = [{name: 'Örnek'}];
+
+
+  add(selectedSection ,event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    
+    // Add our fruit
+    if (value) {
+        this.skills.push({name: value});
+
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(selectedSection,fruit: any): void {
+    if(selectedSection ==0){
+      const index = this.skills.indexOf(fruit);
+
+      if (index >= 0) {
+        this.skills.splice(index, 1);
+  
+        this.announcer.announce(`Removed ${fruit}`);
+      }
+
+    }
+
+  }
+
+  edit(selectedSection,fruit: any, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.remove(selectedSection,fruit);
+      return;
+    }
+
+    // Edit existing fruit
+    if(selectedSection ==0){
+    const index = this.skills.indexOf(fruit);
+    if (index >= 0) {
+      this.skills[index].name = value;
+    }
+  }
+  }
 }
