c1180e988d226a62ce70d47b796a448a88606a08
smlozaydin <samil.bilal.ozaydin@gmail.com>
2024-05-22 18:32:06 +0300

    added UI skill-tests

---
+++ Talento-Frontend/src/app/ui/components/skill-tests/skill-tests.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-tests',
+  templateUrl: './skill-tests.component.html',
+  styleUrls: ['./skill-tests.component.scss']
+})
+export class SkillTestsComponent {
+
+}
---
+++ Talento-Frontend/src/app/ui/components/skill-tests/skill-tests.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SkillTestsComponent } from './skill-tests.component';
+
+
+
+@NgModule({
+  declarations: [
+    SkillTestsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SkillTestsModule { }
