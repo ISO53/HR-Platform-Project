b963e3bf501176aeefcde74b1b630a77f3f93d80
Erdinç Kuvvet <115117751+erdinckuvvet@users.noreply.github.com>
2024-05-23 00:41:29 +0300

    user page designed

--- Talento-Frontend/src/app/ui/components/users/users.component.ts
+++ Talento-Frontend/src/app/ui/components/users/users.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  data: any = {};
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.http.get<any>('http://localhost:8080/users/' + id).subscribe(response => {
+      this.data = response;
+    });
+  }
+
+  updateProfile(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.http.put('http://localhost:8080/users/' + id, this.data).subscribe(response => {
+      console.log('Profile updated successfully');
+    }, error => {
+      console.error('Error updating profile', error);
+    });
+  }
 }
